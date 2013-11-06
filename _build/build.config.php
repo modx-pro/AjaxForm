ca9b9db054ff57bcbc4421b3b4023728fd97d4c5
Bezumkin <bezumkin@yandex.ru>
2013-11-06 11:09:03 +0700

    Version 1.0.0-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -5,7 +5,7 @@ define('PKG_NAME', 'AjaxForm');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
 define('PKG_VERSION', '1.0.0');
-define('PKG_RELEASE', 'rc');
+define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
 /* define paths */
