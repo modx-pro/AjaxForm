39ae4230feded55440125f38c0d9785e64319d72
Bezumkin <bezumkin@yandex.ru>
2014-06-18 15:02:14 +0700

    Version 1.0.2-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,7 +4,7 @@
 define('PKG_NAME', 'AjaxForm');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '1.0.1');
+define('PKG_VERSION', '1.0.2');
 define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
--- _build/build.transport.php
+++ _build/build.transport.php
@@ -308,6 +308,9 @@ if (defined('PKG_AUTO_INSTALL') && PKG_AUTO_INSTALL) {
 		$modx->runProcessor('system/clearcache');
 	}
 }
+if (!empty($_GET['download'])) {
+	echo '<script>document.location.href = "/core/packages/' . $signature.'.transport.zip' . '";</script>';
+}
 
 $modx->log(modX::LOG_LEVEL_INFO,"\n<br />Execution time: {$totalTime}\n");
 echo '</pre>';
