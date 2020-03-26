a7bc73a785272c4d7fabe096a72c6c9927494166
Ibochkarev <ivanx86@gmail.com>
2020-03-26 22:56:47 +0600

    Add Ukrainian lexicons

---
+++ core/components/ajaxform/lexicon/uk/default.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+$_lang['ajaxform'] = 'AjaxForm';
+
+$_lang['af_message_close_all'] = 'Закрити все';
+$_lang['af_submit'] = 'Відправити';
+$_lang['af_reset'] = 'Очистити';
+
+$_lang['af_label_name'] = "Ім'я";
+$_lang['af_label_email'] = 'E-mail';
+$_lang['af_label_message'] = 'Повідомлення';
+
+$_lang['af_err_action_ns'] = 'Не вказано ключ форми (action).';
+$_lang['af_err_action_nf'] = 'Не можу знайти вказаного ключа форми (action).';
+$_lang['af_err_chunk_ns'] = 'Не вказано чанк для зворотньої форми.';
+$_lang['af_err_chunk_nf'] = 'Не можу знайти вказаного чанку "[[+name]]" з формою.';
+$_lang['af_err_snippet_ns'] = 'Не вказано сніппету для зворотньої форми.';
+$_lang['af_err_snippet_nf'] = 'Не можу знайти вказаного сніппету "[[+name]]" для обробки форми.';
+$_lang['af_err_has_errors'] = 'Форма містить помилки';
+$_lang['af_success_submit'] = 'Форма успішно відправлена';
---
+++ core/components/ajaxform/lexicon/uk/properties.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$_lang['ajaxform_prop_form'] = 'Чанк з формою для відправлення.';
+$_lang['ajaxform_prop_snippet'] = 'Сніппет, який буде обробляти вказану форму.';
+$_lang['ajaxform_prop_frontend_css'] = 'Файл з css стилями для підключення на фронтенді.';
+$_lang['ajaxform_prop_frontend_js'] = 'Файл с javascript для підключення на фронтенді.';
+$_lang['ajaxform_prop_actionUrl'] = 'Конектор для обробки ajax запитів.';
+$_lang['ajaxform_prop_formSelector'] = "Им'я CSS класу, якого буде використано як jQuery селектор для ініціалізації форми. Усталене значення 'ajax_form'.";
+$_lang['ajaxform_prop_objectName'] = "Им'я об'єкту для ініціалізації у підключаємому javascript. Усталене значення 'AjaxForm'.";
