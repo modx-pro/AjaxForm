9872c705cb087b43b62de2ff02fa77cf13633ec4
gvozdb <pavelgvozdb@yandex.ru>
2021-09-23 18:36:25 +0600

    1.2.1

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,7 +4,7 @@
 define('PKG_NAME', 'AjaxForm');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '1.2.0');
+define('PKG_VERSION', '1.2.1');
 define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
